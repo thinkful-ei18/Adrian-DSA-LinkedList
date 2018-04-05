75d774ca6c62edee4823bd01b0ffcff025fa617b
Adrian <adriantoddross@gmail.com>
2018-04-05 11:28:03 -0400

    add comments

---
+++ node.js
@@ -0,0 +1,8 @@
+'use strict';
+class _Node {
+  // an object with 2 properties.
+  constructor(value, next) {
+    this.value=value, // value of the node
+    this.next=next; //  memory address of the next node
+  }
+}
