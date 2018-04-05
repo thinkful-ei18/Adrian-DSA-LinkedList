9bca4604296930b207b64cfc395904f91eadb559
Adrian <adriantoddross@gmail.com>
2018-04-05 11:29:34 -0400

    change this.next comment

--- node.js
+++ node.js
@@ -3,6 +3,6 @@ class _Node {
   // an object with 2 properties.
   constructor(value, next) {
     this.value=value, // value of the node
-    this.next=next; //  memory address of the next node
+    this.next=next; //  memory address of the next node or NULL
   }
 }
